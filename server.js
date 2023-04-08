f2c19c25d0fdaea4c3ba7cbf39bb7eea8dc31c6d
joohwancode <jhpark4947@gmail.com>
2023-04-08 23:52:17 +0900

    뷰티용품 페이지

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.listen();
+app.listen(8080, function () {
+  console.log('listening on 8080');
+});
+
+//누군가가/pet으로 방문을 하면 ...
+//pet관련된 안내문을 띄워주자
+app.get('/pet', function (요청, 응답) {
+  응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
+});
+
+app.get('/beauty', function (요청, 응답) {
+  응답.send('뷰티용품 쇼핑 페이지임');
+});
